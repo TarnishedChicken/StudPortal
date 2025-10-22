a23026dc6355030a2308c36af3b447615ade29b7
Otternam <239063574+OtternamThe2nd@users.noreply.github.com>
2025-10-22 23:36:04 +0800

    Added placeholder scripts and cookiehandlers
    added a class in order to save user info as well as placeholder to dynamically insert them into the webpage

---
+++ Version 0.0.4/Functions/cookiehandler.js
@@ -0,0 +1,34 @@
+export default class CookieHandler{
+    constructor(){
+        this.caches=new Map()
+    }
+    setCookie(varKey, val,expDays,toCache=true){
+        const d=new Date()
+        d.setTime(d.getTime()+toMs(expDays))
+        document.cookie=`${varKey}=${val}; expires=${d.toUTCString()}; path=/`
+        if(toCache) this.caches.set(varKey,val)
+    }
+    getCookie(varKey, toCache=false){
+        let cookies=decodeURIComponent(document.cookie).split(";")
+        for(let cookie of cookies){
+            let ci=cookie.indexOf(varKey)
+            if(ci==-1) continue
+            let cval=cookie.substring(ci+1,cookie.length)
+            if(toCache) this.caches.set(keyVal,cval)
+            return cval
+        }
+        if(toCache) this.caches.set(keyVal,null)
+        return null
+    }
+    getCache(varKey){
+        return this.caches.get(varKey)
+    }
+    toMs(days){
+        return days*24*60*60*1000
+    }
+    initCookies(varKeys){
+        for(let varKey of varKeys){
+            this.getCookie(varKey,true)
+        }
+    }
+}
